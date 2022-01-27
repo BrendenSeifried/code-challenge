015065d0e5f9c36ac8e18df0d896c6648ddd256d
BrendenSeifried <brendenseifried@gmail.com>
2022-01-27 12:59:24 -0800

    submitting

--- test/functions.test.js
+++ test/functions.test.js
@@ -30,11 +30,16 @@ const { test, skip } = QUnit;
 
 test('this test should pass', (expect) => {
     const expected = 14;
-    //const expectedTwo = 
+    const expectedTwo = 35;
+    const expectedThree = 21;
 
     const actual = multiplyBySeven(2);
+    const actualTwo = multiplyBySeven(5);
+    const actualThree = multiplyBySeven(3);
 
     expect.equal(actual, expected);
+    expect.equal(actualTwo, expectedTwo);
+    expect.equal(actualThree, expectedThree);
 });
 
 
@@ -50,27 +55,45 @@ skip('this test should be skipped', (expect) => {
 
 test('this test should pass', (expect) => {
     const expected = 12;
+    const expectedTwo = 60;
+    const expectedThree = 18;
 
     const actual = multiplyBy12ThenHalve(2);
+    const actualTwo = multiplyBy12ThenHalve(10);
+    const actualThree = multiplyBy12ThenHalve(3);
 
     expect.equal(actual, expected);
+    expect.equal(actualTwo, expectedTwo);
+    expect.equal(actualThree, expectedThree);
 });
 
 
 test('this test should pass', (expect) => {
     const expected = 'testing!!!';
+    const expectedTwo = 'Hello World!!!';
+    const expectedThree = 'Go Browns!!!';
 
     const actual = addExclamationPoints('testing');
+    const actualTwo = addExclamationPoints('Hello World');
+    const actualThree = addExclamationPoints('Go Browns');
 
     expect.equal(actual, expected);
+    expect.equal(actualTwo, expectedTwo);
+    expect.equal(actualThree, expectedThree);
 });
 
 test('this test should pass', (expect) => {
     const expected = 10;
+    const expectedTwo = 6;
+    const expectedThree = 21;
 
     const actual = divideThenMultiply(8, 4, 5);
+    const actualTwo = divideThenMultiply(9, 3, 2);
+    const actualThree = divideThenMultiply(6, 2, 7);
 
     expect.equal(actual, expected);
+    expect.equal(actualTwo, expectedTwo);
+    expect.equal(actualThree, expectedThree);
 });
 
 test('this test should pass', (expect) => {
@@ -78,7 +101,9 @@ test('this test should pass', (expect) => {
 
     const actual = returnAsAnArray(8, 4, 5);
 
-    expect.deepEqual(actual, expected);
+    expect.equal(actual, expected);
+    expect.equal(actualTwo, expectedTwo);
+    expect.equal(actualThree, expectedThree);
 });
 
 test('this test should pass', (expect) => {
